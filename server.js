0d4728ee0802a6463ac32a164aee5b2a6009a925
Schneider-Joachim <92768254+Schneider-Joachim@users.noreply.github.com>
2023-02-27 23:06:30 -0500

    up

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
+server.search(cors());
+server.use(express.json());
 
 server.get("/", (req, res) => {
     res.send({input: "output"});
